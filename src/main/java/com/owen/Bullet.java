f164f9532f2a4eb58d04d3c1d40d3ada601f4948
owenhuangxj <wangluoke007@163.com>
2022-11-04 05:14:53 +0800

    【Title】处理坦克发射子弹 【DateTime】2022-11-04 【Reviewer】Owen

---
+++ src/main/java/com/owen/Bullet.java
@@ -0,0 +1,49 @@
+package com.owen;
+
+import java.awt.*;
+
+/**
+ * @author OwenHuang
+ * @since 2022/11/3 23:13
+ */
+public class Bullet {
+    private static final int WIDTH = 5;
+    private static final int HEIGHT = 5;
+    private static final int SPEED = 5;
+    private int x, y;
+    private Direction direction;
+
+    public Bullet(int x, int y, Direction direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    private void move() {
+        System.out.println("Tank direction:" + direction + "x:" + x + ",y:" + y);
+        switch (direction) {
+            case UP:
+                y -= SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+            case LEFT:
+                x -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void paint(Graphics graphics) {
+        Color color = graphics.getColor();
+        graphics.setColor(Color.RED);
+        graphics.fillOval(x, y, WIDTH, HEIGHT);
+        graphics.setColor(color);
+        move();
+    }
+}
